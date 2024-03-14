2568c3a5e410c929b82ffd09ab4f6d5fb189ca72
Ashik Kabeer <97529548+ashikkabeer@users.noreply.github.com>
2024-03-14 06:43:09 +0530

    exercise

---
+++ src/LeetCodeProblems/Main.java
@@ -0,0 +1,24 @@
+package LeetCodeProblems;
+
+public class Main {
+    public static void main(String[] args) {
+        int ans = pivotInteger(8);
+        System.out.println(ans);
+    }
+        public static int pivotInteger(int n) {
+            int leftBound = 1, rightBound = n;
+            int fullSum = (n * (n + 1)) / 2;
+            while (leftBound <= rightBound) {
+                int midNum = leftBound + (rightBound - leftBound) / 2;
+                int firstHlfSum = (midNum * (midNum + 1)) / 2;
+                int secondHlfSum = fullSum - firstHlfSum + midNum;
+                if (firstHlfSum == secondHlfSum)
+                    return midNum;
+                else if (firstHlfSum < secondHlfSum)
+                    leftBound = midNum + 1;
+                else
+                    rightBound = midNum - 1;
+            }
+            return -1;
+        }
+    }
